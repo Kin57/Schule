4ad2cacc3d0a0c3172dff2744d86fb7beec90f57
Kin57 <a.kreuzer@students.htl-leonding.ac.at>
2022-09-26 13:31:56 +0200

    huehue

---
+++ Java/mult.java
@@ -0,0 +1,12 @@
+public class mult{
+    public static void main(String[] args){
+        int mult1 = 4;
+        int mult2 = 8;
+        int temp = 0;
+
+        for(int i = 0; i < mult1; i++){
+            temp += mult2;
+        } 
+        System.out.println(temp);
+    }
+}
---
+++ Java/mult.py
@@ -0,0 +1,8 @@
+mult1 = 4
+mult2 = 8
+temp = 0
+
+for i in range(mult1):
+    temp += mult2
+
+print(temp)
