66d5aeb62fca00394a07fd5014c82a8e897f0c54
Kin57 <a.kreuzer@students.htl-leonding.ac.at>
2022-10-18 12:27:55 +0200

    OnceUponATime

--- html/DiceBattle/DiceBattle.js
+++ html/DiceBattle/DiceBattle.js
@@ -1,23 +1,41 @@
+let output = document.getElementById("output");
 let winner = document.getElementById("winner");
-winner.innerHTML += "<p>test1</p>"
-function newFight() {
-    let life1 = 13;
-    let life2 = 13;
+let lifebar1 = document.getElementById("lifebar1");
+let lifebar2 = document.getElementById("lifebar2");
 
-    do{
+function newFight() {
+    let life1 = 100;
+    let life2 = 100;
+
+    output.innerHTML = '';
+/*
+    do {
         life1 = attack(life1);
+        lifebar1.innerHTML = '<div style="background: rgb(0,0,0); background: linear-gradient(90deg, rgba(0,0,0,1) '+ life1 +'%, rgba(151,0,0,1) 50%);"></div>';
         if (life1 > 0) {
             life2 = attack(life2);
+            lifebar2.innerHTML = '<div style="background: rgb(151,0,0); background: linear-gradient(90deg, rgba(0,0,0,1) '+ 100-life2 +'%, rgba(0,0,0,1) 50%);"></div>';
         } else {
-            winner.innerHTML = "<p>Player 2 Wins</p>";
+            winner.innerHTML += "<p>Player 2 Wins</p>";
         }
-    }while(life2 > 0);
-    winner.innerHTML = "<p>Player 1 Wins</p>";
+    } while (life2 > 0);
+    winner.innerHTML += "<p>Player 1 Wins</p>";*/
 }
 
-function attack(life){
-    winner.innerHTML += "<p>testattack</p>"
-    return life-newDice();
+function nextattack() {
+    roll1 = newDice();
+    player1.innerHTML += '<img src="">';
+    
+    roll2 = newDice();
+    player1.innerHTML += '<img src="">';
+
+    if(roll1 > roll2){
+        life2 - attack2
+    }else if(roll1 < roll2){
+        life1 - attack2
+    }else{
+
+    }
 }
 
 function newDice() {
