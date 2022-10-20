7ea85554d2229f30e7eb03db2d5ba72329a10620
Kin57 <a.kreuzer@students.htl-leonding.ac.at>
2022-10-20 19:45:25 +0200

    hotLabdob

--- html/DiceBattle/DiceBattle.js
+++ html/DiceBattle/DiceBattle.js
@@ -1,40 +1,35 @@
 let output = document.getElementById("output");
-let winner = document.getElementById("winner");
-let lifebar1 = document.getElementById("lifebar1");
-let lifebar2 = document.getElementById("lifebar2");
+let button = document.getElementById("button");
 
 function newFight() {
     let life1 = 100;
     let life2 = 100;
+    output.innerHTML = '<div id="player1"><div id="lifebar1"></div><div><div id="dice1"></div></div><div id="player2"><div id="lifebar2"></div><div id="dice2"></div></div><div id="winner"></div>';
+    button.innerHTML = '<button onclick="nextAttack();">NEW GAME</button>';
 
-    output.innerHTML = '';
-/*
-    do {
-        life1 = attack(life1);
-        lifebar1.innerHTML = '<div style="background: rgb(0,0,0); background: linear-gradient(90deg, rgba(0,0,0,1) '+ life1 +'%, rgba(151,0,0,1) 50%);"></div>';
-        if (life1 > 0) {
-            life2 = attack(life2);
-            lifebar2.innerHTML = '<div style="background: rgb(151,0,0); background: linear-gradient(90deg, rgba(0,0,0,1) '+ 100-life2 +'%, rgba(0,0,0,1) 50%);"></div>';
-        } else {
-            winner.innerHTML += "<p>Player 2 Wins</p>";
-        }
-    } while (life2 > 0);
-    winner.innerHTML += "<p>Player 1 Wins</p>";*/
+    let winner = document.getElementById("winner");
+    let lifebar1 = document.getElementById("lifebar1");
+    let lifebar2 = document.getElementById("lifebar2");
+    let dice1 = document.getElementById("dice1");
+    let dice2 = document.getElementById("dice2");
 }
 
-function nextattack() {
-    roll1 = newDice();
-    player1.innerHTML += '<img src="">';
-    
-    roll2 = newDice();
-    player1.innerHTML += '<img src="">';
+function nextAttack() {
+    let roll1 = newDice();
+    dice1.innerHTML = '<img src="img/PinClipart_cube_'+roll1+'.png">';
+    let roll2 = newDice();
+    dice2.innerHTML = '<img src="img/PinClipart_cube_'+roll2+'.png">';
 
     if(roll1 > roll2){
-        life2 - attack2
+        life2-attack1;
+        lifebar2.innerHTML = '<div style="background: rgb(151,0,0); background: linear-gradient(90deg, rgba(0,0,0,1) '+ 100-life2 +'%, rgba(0,0,0,1) 50%);"></div>';
+        winner.innerHTML = 'Player 1 wins';
     }else if(roll1 < roll2){
-        life1 - attack2
+        life1-attack2;
+        lifebar1.innerHTML = '<div style="background: rgb(0,0,0); background: linear-gradient(90deg, rgba(0,0,0,1) '+ life1 +'%, rgba(151,0,0,1) 50%);"></div>';
+        winner.innerHTML = 'Player 2 wins';
     }else{
-
+        winner.innerHTML = 'Draw';
     }
 }
 
