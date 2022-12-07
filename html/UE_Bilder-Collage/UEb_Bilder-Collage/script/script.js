325330315fe171e626a0f76b9b2c71ff69b80707
Kin57 <a.kreuzer@students.htl-leonding.ac.at>
2022-12-07 18:22:37 +0100

    bashScript

--- html/UE_Bilder-Collage/UEb_Bilder-Collage/script/script.js
+++ html/UE_Bilder-Collage/UEb_Bilder-Collage/script/script.js
@@ -9,17 +9,17 @@ showMore()
 
 function showMore() {
     if (counter > 15) {
-        button.innerHTML = '<p id="sry">Das war leider alles</p>'
+        document.getElementById("button").innerHTML = '<p id="sry">Das war leider alles</p>';//wenn ich die variable nutze gehts nicht, drum hab ichs so
     } else {
         console.log(counter)
-        document.getElementById("button").innerHTML = '<button id="showMeMore" onclick="showMore()">mehr Bilder</button>';
-        //wenn ich die variable nutze gehts nicht, drum hab ichs so
-    }
-    content.innerHTML += `<div class="row" id="${i}"></div>`;
-    for (let j = 0; j < 3; j++) {
-        let img = document.getElementById(`${i}`);
-        img.innerHTML += `<img class="column${j}" src="img/city${counter}.jpg">`;
-        counter++;
+        document.getElementById("button").innerHTML = '<button id="showMeMore" onclick="showMore()">mehr Bilder</button>';//wenn ich die variable nutze gehts nicht, drum hab ichs so
+        content.innerHTML += `<div class="row" id="${i}"></div>`;
+        for (let j = 0; j < 3; j++) {
+            console.log(counter)
+            let img = document.getElementById(`${i}`);
+            img.innerHTML += `<img class="column${j}" src="img/city${counter}.jpg">`;
+            counter++;
+        }
     }
     i++
 }
