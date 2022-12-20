95e2c0c56f5237af39c739a32e1a12bff7baaf54
Kin57 <a.kreuzer@students.htl-leonding.ac.at>
2022-12-20 07:51:50 +0100

    bashScript

--- MEDT/UE9_Themenseite/script.js
+++ MEDT/UE9_Themenseite/script.js
@@ -1,29 +1,38 @@
 let content = document.getElementById("content");
-let output = '';
 const MUSHROOM_ARRAY = [
-    ['Trüffel', 0, '', 'img/trueffel.jpg'],
-    ['Champignons', 0, '', 'img/champignons.jpg'],
-    ['Fliegenpilz', 0, '', 'img/fliegenpilz.jpg'],
-    ['name', 12, '', 'img/test.jpg'],
-    ['name', 17, 'lalala', 'img/test.jpg']
+    ['Trüffel', 'img/trueffel.jpg', false, 'idk never tried it', 'Trüffel-Gericht'],
+    ['Trüffel', 'img/trueffel.jpg', false, 'idk never tried it', 'Trüffel-Gericht'],
+    ['Trüffel', 'img/trueffel.jpg', false, 'idk never tried it', 'Trüffel-Gericht'],
+    ['Trüffel', 'img/trueffel.jpg', false, 'idk never tried it', 'Trüffel-Gericht'],
+    ['Trüffel', 'img/trueffel.jpg', false, 'idk never tried it', 'Trüffel-Gericht'],
 ]
 
 
 for(let i = 0; i < MUSHROOM_ARRAY.length; i++){
     if(i % 3 === 0){
         console.log('newRow()')
-        content += '<div class="row" id="row'+ i +'"></div>';
-    }
-    newCard(MUSHROOM_ARRAY[i][0], MUSHROOM_ARRAY[i][1], MUSHROOM_ARRAY[i][2], MUSHROOM_ARRAY[i][3], i);
+        content.innerHTML += '<div class="row" id="row'+ i +'"></div>';
+     }
+    newCard(MUSHROOM_ARRAY[i][0], MUSHROOM_ARRAY[i][1], MUSHROOM_ARRAY[i][2], MUSHROOM_ARRAY[i][3], MUSHROOM_ARRAY[i][4], i);
 }
 
 
-function newCard(name, price, text, imgSrc, i){
+function newCard(name, imgSrc, isPoisonous, taste, exampleDish, i){
     console.log('newCard()')
+    let color = green
+    if(isPoisonous){
+        color = red
+    }
     document.getElementById('row'+i).innerHTML += '' +
         '<div class="card" id="img'+ i +'">' +
         '  <div><img src="'+ imgSrc +'"></div>' +
         '  <h1>'+ name +'</h1>' +
-        '  <p>'+ text +'</p>' +
+        '  <p>'+ taste +'</p>' +
+        '  <p>'+ exampleDish +'</p>' +
         '</div>';
 }
+
+
+
+
+//'  <div style="background: '+ color +'" id="dangerSign"></div>' +
